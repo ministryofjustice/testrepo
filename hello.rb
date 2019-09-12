9658c7c956e21a5f3e4e223a863780c370c09ebc
David Salgado <david@digitalronin.com>
2019-09-12 16:02:45 +0100

    add 'query' outer key

--- hello.rb
+++ hello.rb
@@ -20,36 +20,38 @@ puts "Hello from ruby"
 # puts "Github event..."
 # puts File.read("/home/runner/work/_temp/_github_workflow/event.json")
 
-query = <<EOF
+query = %[
 {
-  resource(url: "https://github.com/ministryofjustice/testrepo/issues/15") {
-    ... on Issue {
-      projectCards {
-        nodes {
-          id
-        }
-      }
-      repository {
-        projects(search: "testproject", first: 10, states: [OPEN]) {
+  query {
+    resource(url: "https://github.com/ministryofjustice/testrepo/issues/15") {
+      ... on Issue {
+        projectCards {
           nodes {
             id
-            columns(first: 100) {
-              nodes {
-                id
-                name
+          }
+        }
+        repository {
+          projects(search: "testproject", first: 10, states: [OPEN]) {
+            nodes {
+              id
+              columns(first: 100) {
+                nodes {
+                  id
+                  name
+                }
               }
             }
           }
-        }
-        owner {
-          ... on Organization {
-            projects(search: "testproject", first: 10, states: [OPEN]) {
-              nodes {
-                id
-                columns(first: 100) {
-                  nodes {
-                    id
-                    name
+          owner {
+            ... on Organization {
+              projects(search: "testproject", first: 10, states: [OPEN]) {
+                nodes {
+                  id
+                  columns(first: 100) {
+                    nodes {
+                      id
+                      name
+                    }
                   }
                 }
               }
@@ -60,6 +62,6 @@ query = <<EOF
     }
   }
 }
-EOF
+]
 
 puts run_query(query)
